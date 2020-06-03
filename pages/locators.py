47b935d4c3e3ba5e242a5b9e98e6dc800972b9b7
Гимадеев Камиль Габдуллазянович <kamil.gimadeev@rtlabs.ru>
2020-06-03 17:55:09 +0400

    Updated product page tests

--- pages/locators.py
+++ pages/locators.py
@@ -1,11 +1,12 @@
 from selenium.webdriver.common.by import By
-
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR , '#login_link')
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
     PRICE = (By.CSS_SELECTOR, '.price_color')
--- pages/product_page.py
+++ pages/product_page.py
@@ -16,3 +16,11 @@ class ProductPage(BasePage):
         mess = self.browser.find_elements(*ProductPageLocators.MESSAGE)
         mess0 = mess[0].text
         assert (a in b) and (prodname == mess0), 'Product was not added'
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_not_be_success_message_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.MESSAGE), \
+            "Element was not disappeared"
